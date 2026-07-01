1bc2694d3f92191ec55fce13da6b994d2adc6274
Sean Murphy <sean@iamseanmurphy.com>
2026-07-01 13:14:36 +0000

    4.2.7

--- neve/vendor/composer/installed.php
+++ neve/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/neve',
-        'pretty_version' => 'v4.2.6',
-        'version' => '4.2.6.0',
-        'reference' => 'e86c860f624b51bceb0e16bf542ac631002013d8',
+        'pretty_version' => 'v4.2.7',
+        'version' => '4.2.7.0',
+        'reference' => '5d07e1be8edb9839f39d47b86a48efa9b92dfdbf',
         'type' => 'wordpress-theme',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/neve' => array(
-            'pretty_version' => 'v4.2.6',
-            'version' => '4.2.6.0',
-            'reference' => 'e86c860f624b51bceb0e16bf542ac631002013d8',
+            'pretty_version' => 'v4.2.7',
+            'version' => '4.2.7.0',
+            'reference' => '5d07e1be8edb9839f39d47b86a48efa9b92dfdbf',
             'type' => 'wordpress-theme',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.53',
-            'version' => '3.3.53.0',
-            'reference' => 'a657eaedf62cc84c82e539167ac9e19b3e618ce8',
+            'pretty_version' => '3.3.54',
+            'version' => '3.3.54.0',
+            'reference' => '095c2d0f1388af0b0196c492a7f79e2fd092dab1',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
